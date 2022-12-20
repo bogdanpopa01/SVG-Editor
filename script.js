f25a9e5db56f39a410fc2fbe27d93d332fc4dfb7
Bogdan Popa <90871325+bogdanpopa01@users.noreply.github.com>
2022-12-20 20:33:28 +0200

    cleaner code v2

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
-document.addEventListener("DOMContentLoaded", myApp);
-
 function myApp() {
   let mySvg = document.querySelector("#mySvg");
   let mainHeader = document.querySelector("#main-header");
 
+  let line = document.querySelector("#line");
   // it is actually a rectangle
   let square = document.querySelector("#square");
   let circle = document.querySelector("#circle");
-  let line = document.querySelector("#line");
+  let color = document.querySelector("#spinnerColor");
 
-  let value = "square";
-  let select = document.querySelector("#spinnerColor");
-
-  elemente = document.querySelector("#elemente");
+  let btnLine = document.querySelector("#btnLine");
   let btnSquare = document.querySelector("#btnSquare");
   let btnCircle = document.querySelector("#btnCircle");
-  let btnLine = document.querySelector("#btnLine");
   let btnEraseAll = document.querySelector("#btnEraseAll");
+  let elements = document.querySelector("#elements");
 
   function drawSquare() {
+    console.log("Rectangle");
     value = "square";
-    console.log("Square");
   }
 
   function drawCircle() {
-    value = "circle";
     console.log("Circle");
+    value = "circle";
   }
 
   function drawLine() {
-    value = "line";
     console.log("Line");
-  }
-
-  function eraseAll() {
-    while (elemente.children.length > 0) {
-      elemente.children[0].remove();
-    }
-    console.log("EraseAll button");
+    value = "line";
   }
 
   function selectedColor() {
-    let x = select.value;
     console.log("Selected color");
+    let value = color.value;
   }
 
+  function eraseAll() {
+    console.log("EraseAll button");
+    while (elements.children.length > 0) {
+      elements.children[0].remove();
+    }
+  }
+
+  btnLine.addEventListener("click", drawLine);
   btnSquare.addEventListener("click", drawSquare);
   btnCircle.addEventListener("click", drawCircle);
-  btnLine.addEventListener("click", drawLine);
   btnEraseAll.addEventListener("click", eraseAll);
 
-  mySvg.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-  });
-
-  let selected = { valoare: value, tip: null };
+  let value = "square";
+  let obj = { val: value, type: null };
   let mouseMovement = false;
   let x1 = 0,
     y1 = 0,
     myX = 0,
     myY = 0;
+
   function draw() {
     if (mouseMovement) {
-      if (value === "square") {
+      if (value === "line") {
+        line.style.display = "block";
+        line.setAttributeNS(null, "x1", x1);
+        line.setAttributeNS(null, "x2", myX);
+        line.setAttributeNS(null, "y1", y1);
+        line.setAttributeNS(null, "y2", myY);
+      } else if (value === "square") {
         square.style.display = "block";
         square.setAttributeNS(null, "x", Math.min(x1, myX));
         square.setAttributeNS(null, "y", Math.min(y1, myY));
@@ -77,21 +77,16 @@ function myApp() {
           "r",
           Math.max(Math.abs(myX - x1), Math.abs(myY - y1))
         );
-      } else if (value === "line") {
-        line.style.display = "block";
-        line.setAttributeNS(null, "x1", x1);
-        line.setAttributeNS(null, "y1", y1);
-        line.setAttributeNS(null, "x2", myX);
-        line.setAttributeNS(null, "y2", myY);
       }
     } else {
+      line.style.display = "none";
       square.style.display = "none";
       circle.style.display = "none";
-      line.style.display = "none";
     }
     requestAnimationFrame(draw);
   }
   draw();
+
   mySvg.addEventListener("mousemove", (e) => {
     myX = e.clientX - mySvg.getBoundingClientRect().left;
     myY = e.clientY - mySvg.getBoundingClientRect().top;
@@ -112,29 +107,43 @@ function myApp() {
       return;
     }
 
-    if (value === "square") {
+    if (value === "line") {
+      let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      line.style.display = "block";
+      line.setAttributeNS(null, "x1", x1);
+      line.setAttributeNS(null, "x2", myX);
+      line.setAttributeNS(null, "y1", y1);
+      line.setAttributeNS(null, "y2", myY);
+      elements.append(line);
+
+      line.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        obj.val = e.target.tagName;
+        obj.type = e.target;
+      });
+    } else if (value === "square") {
       let square = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "rect"
       );
-      square.style.fill = select.value;
+      square.style.fill = color.value;
       square.setAttributeNS(null, "x", Math.min(x1, myX));
       square.setAttributeNS(null, "y", Math.min(y1, myY));
-      square.setAttributeNS(null, "width", Math.abs(myX - x1));
       square.setAttributeNS(null, "height", Math.abs(myY - y1));
-      elemente.append(square);
+      square.setAttributeNS(null, "width", Math.abs(myX - x1));
+      elements.append(square);
 
       square.addEventListener("contextmenu", (e) => {
         e.preventDefault();
-        selected.tip = e.target;
-        selected.valoare = e.target.tagName;
+        obj.val = e.target.tagName;
+        obj.type = e.target;
       });
     } else if (value === "circle") {
       let circle = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "circle"
       );
-      circle.style.fill = select.value;
+      circle.style.fill = color.value;
       circle.setAttributeNS(null, "cx", x1);
       circle.setAttributeNS(null, "cy", y1);
       circle.setAttributeNS(
@@ -143,36 +152,23 @@ function myApp() {
         Math.max(Math.abs(myX - x1), Math.abs(myY - y1))
       );
 
-      elemente.append(circle);
+      elements.append(circle);
 
       circle.addEventListener("contextmenu", (e) => {
         e.preventDefault();
-        selected.tip = e.target;
-        selected.valoare = e.target.tagName;
-      });
-    } else if (value === "line") {
-      let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-      line.style.display = "block";
-      line.setAttributeNS(null, "x1", x1);
-      line.setAttributeNS(null, "y1", y1);
-      line.setAttributeNS(null, "x2", myX);
-      line.setAttributeNS(null, "y2", myY);
-      elemente.append(line);
-
-      line.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-        selected.tip = e.target;
-        selected.valoare = e.target.tagName;
+        obj.val = e.target.tagName;
+        obj.type = e.target;
       });
     }
-
     mouseMovement = false;
   });
 
   document.addEventListener("keydown", (e) => {
-    if (e.key === "d" && selected !== null) {
-      selected.tip.remove();
-      selected.tip = null;
+    if (e.key === "d" && obj !== null) {
+      obj.type.remove();
+      obj.type = null;
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", myApp);
